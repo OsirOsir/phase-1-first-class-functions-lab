1717afb215f8de6748bf1dbb8e725e8ad0a5c1de
Philip osir Otieno <philiposir2@gmail.com>
2024-06-28 01:01:53 +0300

    add more functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+  return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']) {
+  return drivers.slice(2, 4)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(intagerMultiplier) {
+  return function(fare) {
+    return  intagerMultiplier * fare 
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnLastTwoDrivers) {
+  return returnLastTwoDrivers(drivers);
+}
